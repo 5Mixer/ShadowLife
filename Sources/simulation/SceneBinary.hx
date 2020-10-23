2c3f2bd37e7aa5cdb0292a180610dda0ad011d50
Daniel Blaker <daniel.blaker@student.unimelb.edu.au>
2020-10-23 16:15:21 +1100

    Binary scene decoding

--- Sources/simulation/SceneBinary.hx
+++ Sources/simulation/SceneBinary.hx
@@ -1,5 +1,8 @@
 package simulation;
 
+import simulation.actor.*;
+import simulation.actor.character.*;
+import kha.math.Vector2i;
 import haxe.io.Bytes;
 import simulation.actor.Actor;
 
@@ -15,13 +18,13 @@ class SceneBinary {
             case Pad: 6;
             case Sign: 
                 switch (cast(actor,simulation.actor.Sign).direction) {
-                    case UP: 6;
-                    case DOWN: 7;
-                    case LEFT: 8;
-                    case RIGHT: 9;
+                    case UP: 7;
+                    case DOWN: 8;
+                    case LEFT: 9;
+                    case RIGHT: 10;
                 }
-            case Stockpile: 10;
-            case Tree: 11;
+            case Stockpile: 11;
+            case Tree: 12;
         }
     }
 
@@ -39,11 +42,16 @@ class SceneBinary {
 
         var byteOffset = 0;
         for (actor in actors) {
-            bytes.set(byteOffset, getActorCode(actor));
+            var largePosition = actor.position.x > 255 || actor.position.y > 255;
+            var actorCode = getActorCode(actor);
+            if (largePosition) {
+                actorCode |= 1<<7;
+            }
+            bytes.set(byteOffset, actorCode);
             byteOffset++;
 
             // For positions that require more than a byte, use two bytes, otherwise use single bytes
-            if (actor.position.x > 255 || actor.position.y > 255) {
+            if (largePosition) {
                 bytes.setUInt16(byteOffset, actor.position.x);
                 byteOffset += 2;
                 bytes.setUInt16(byteOffset, actor.position.y);
@@ -57,5 +65,61 @@ class SceneBinary {
         }
 
         return bytes;
+    }
+
+    // Mutates bytes when unflipping large position bit
+    public static function fromBytes(bytes:Bytes) {
+        if (bytes.length == 0)
+            return [];
+
+        var actors = new Array<Actor>();
+        var byteOffset = 0;
+        while (byteOffset < bytes.length) {
+            var actorCode = bytes.get(byteOffset);
+            byteOffset++;
+
+            var largePosition = actorCode & (1 << 7) != 0;
+            actorCode = actorCode & (15); // Preverse 4 LSB, 15d = 1111b
+
+            var position:Vector2i;
+            if (largePosition) {
+                var x = bytes.getUInt16(byteOffset);
+                byteOffset += 2;
+                var y = bytes.getUInt16(byteOffset);
+                byteOffset += 2;
+
+                position = new Vector2i(x,y);
+            }else{
+                var x = bytes.get(byteOffset);
+                byteOffset++;
+                var y = bytes.get(byteOffset);
+                byteOffset++;
+
+                position = new Vector2i(x,y);
+            }
+
+            var actor:Actor;
+            actor = switch(actorCode) {
+                case 0: new Gatherer(position);
+                case 1: new Thief(position);
+                case 2: new Fence(position);
+                case 3: new GoldenTree(position);
+                case 4: new Hoard(position);
+                case 5: new MitosisPool(position);
+                case 6: new Pad(position);
+
+                case 7: new Sign(position, UP);
+                case 8: new Sign(position, DOWN);
+                case 9: new Sign(position, LEFT);
+                case 10: new Sign(position, RIGHT);
+                
+                case 11: new Stockpile(position);
+                case 12: new Tree(position);
+
+                case _: null;
+            }
+            actors.push(actor);
+        }
+        return actors;
     }
 }
